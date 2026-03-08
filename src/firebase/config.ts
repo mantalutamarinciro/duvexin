c167ea8df9432df659fd7c2badb3de3bb1168246
Marin Mantaluta <mantalutamarinciro@gmail.com>
2026-03-08 17:09:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "movesmart-dashboard",
+  "appId": "1:167776442405:web:efc36eba660e42eb53c7cc",
+  "apiKey": "AIzaSyDOAbetidHcbpOXEZlG9vIQVenSenF69OE",
+  "authDomain": "movesmart-dashboard.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "167776442405"
+};
